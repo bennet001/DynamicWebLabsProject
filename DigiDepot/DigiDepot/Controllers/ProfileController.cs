de8c9f6cbf313dbef251937eb92aaf4370b71619
Zack Austin <cozackility@live.com>
2015-05-22 15:42:32 -0600

    Revert "Revert "Profile Page""
    This reverts commit f6867d11fb0a0dbf9fb65c653060a345e2675706.

---
+++ DigiDepot/DigiDepot/Controllers/ProfileController.cs
@@ -0,0 +1,51 @@
+﻿using DigiDepot.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DigiDepot.Controllers
+{
+    public class ProfileController : Controller
+    {
+        private User hardCodedUser = new User("zaustin@student.neumont.edu", "zach", "austin", "350S 600E", "Apt. 406", "Salt Lake City", "UT", 84102);
+
+        public ActionResult UserPage()
+        {
+            //get the user info from the session if it exists or from a database based on id 
+            //we know which id to use because the user is logged in
+            //alternately, hard code a user for demonstration
+            if (System.Web.HttpContext.Current.Session["currentUser"] == null)
+            {
+                System.Web.HttpContext.Current.Session["currentUser"] = hardCodedUser;
+            }
+            return View(System.Web.HttpContext.Current.Session["currentUser"]);
+        }
+
+        public ActionResult EditUser()
+        {
+            //we need a user object to pre populate fields in the edit form
+            //so we get it from the session if it exists or get it from the database otherwise
+            //again, "get it from the db" means hard code it, because we dont have a db yet
+            if (System.Web.HttpContext.Current.Session["currentUser"] == null)
+            {
+                System.Web.HttpContext.Current.Session["currentUser"] = hardCodedUser;
+            }
+            return View(System.Web.HttpContext.Current.Session["currentUser"]);
+        }
+
+        [HttpPost]
+        public ActionResult UpdateProfile(string firstname, string lastname, string address, string address2, string city, string state, int zip)
+        {
+            //code to contact the database and update the user
+            //store the updated user object in the session
+            //for the demo, as we are sans-DB, I construct a new User object using form data and replace the session data with that
+
+            User current = (User)System.Web.HttpContext.Current.Session["currentUser"];
+            User updated = new User(current.EmailAddress, firstname, lastname, address, address2, city, state, zip);
+            System.Web.HttpContext.Current.Session["currentUser"] = updated;
+            return Redirect("UserPage");//redirect to the profile page
+        }
+    }
+}
---
+++ DigiDepot/DigiDepot/Models/User.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DigiDepot.Models
+{
+    public class User
+    {
+        public long UserId { get; set; }
+        public String EmailAddress { get; set; }
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+        public String Company { get; set; }//Optional
+        public String Address { get; set; }
+        public String Address2 { get; set; }//apt#, suite, etc. Optional
+        public String City { get; set; }
+        public String State { get; set; }
+        public int ZipCode { get; set; }
+
+        public User()
+        {
+
+        }
+
+        public User(string email, string first, string last, string address, string address2, string city, string state, int zipcode)
+        {
+            EmailAddress = email;
+            FirstName = first;
+            LastName = last;
+            Address = address;
+            Address2 = address2;
+            City = city;
+            State = state;
+            ZipCode = zipcode;
+        }
+    }
+}
